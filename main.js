172ee2b126952eeaeb438b6c8efe5b8b35b1a3b5
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-03-22 21:47:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
+function get(id) {
+	return document.getElementById(id);
+}
 function getDefaultSave() {
 	return {
-		nanites: new Decimal(0),
+		matter: 0,
+		nanites: 1,
+		naniteCost: 10,
+		lastTick: 0,
 	}
+}
+let player = getDefaultSave();
+
+function produce(time) {
+	player.matter += player.nanites * time;
+}
+function gameLoop() {
+	let newTime = new Date().getTime()
+	let diff = (newTime - player.lastTick) / 1000;
+	player.lastTick = newTime;
+	produce(diff);
+}
+function update() {
+	get("matter").innerHTML = Math.floor(player.matter);
+	get("naniteCost").innerHTML = Math.floor(player.naniteCost);
+	get("nanites").innerHTML = Math.floor(player.nanites);
+}
+function buyNanite() {
+	player.nanites++;
+	player.naniteCost *= 1.1;
 }
